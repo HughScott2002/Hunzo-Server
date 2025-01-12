cd957b7d42fa691ff0fc09b4be1ac83786be1671
Hugh Scott <hughscott2002@yahoo.com>
2025-01-12 15:33:42 -0500

    Adding COrs to Wallet

--- 3-wallet/go.mod
+++ 3-wallet/go.mod
@@ -7,6 +7,8 @@ require (
 	github.com/go-redis/redis/v8 v8.11.5
 )
 
+require github.com/go-chi/cors v1.2.1 // indirect
+
 require (
 	github.com/cespare/xxhash/v2 v2.1.2 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
--- 3-wallet/src/server/router.go
+++ 3-wallet/src/server/router.go
@@ -6,6 +6,8 @@ import (
 	"example.com/m/v2/src/server/handlers"
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/cors"
+
 )
 // TODO: GET A LIST OF THE WALLETS AND EACH ATTACHED CARD
 
@@ -13,7 +15,14 @@ func Router() http.Handler {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
-
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"http://localhost:3000"}, // Allow your frontend origin
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           300,
+	}))
 	r.Route("/api/wallets", func(r chi.Router) {
 		//Wallet by account
 		r.Get("/{walletId}", handlers.GetWallet)
